07dbee546b87bafb9d60cf532711f502c0a7330f
ujin_jeong <ujin_jeong@tmax.co.kr>
2024-09-23 20:42:41 +0900

    initial commit

---
+++ src/main/java/com/_0/bbangddeoli/BbangddeoliApplication.java
@@ -0,0 +1,13 @@
+package com._0.bbangddeoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbangddeoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbangddeoliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_0/bbangddeoli/BbangddeoliApplicationTests.java
@@ -0,0 +1,13 @@
+package com._0.bbangddeoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbangddeoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
